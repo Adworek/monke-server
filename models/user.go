f10cd2bd02037c03b3c8d81f1681a5d4f22ff87d
Zero <dakoolstwunn@gmail.com>
2019-11-23 16:55:45 -0500

    User.New method for creating a user

--- models/user.go
+++ models/user.go
@@ -1,5 +1,9 @@
 package models
 
+import (
+	"time"
+)
+
 type User struct {
 	ID                string
 	Nick              string
@@ -8,4 +12,17 @@ type User struct {
 	SubscriptionCount int
 	PostCount         int
 	Created           int64
+}
+
+func (user User) New(nick string, about string, id string) User {
+	var created User = User{
+		ID:                id,
+		Nick:              nick,
+		Bio:               bio,
+		SubscriberCount:   0,
+		SubscriptionCount: 0,
+		Created:           time.Now().Unix(),
+	}
+
+	return created
 }
