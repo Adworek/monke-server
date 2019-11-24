f05d24df0319eb3ec3e0db62eb5452ab3301d89a
Zero <dakoolstwunn@gmail.com>
2019-11-23 23:23:49 -0500

    serving the server on start

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"monke-server/server"
 	"monke-server/storage"
-	"fmt"
+	"net/http"
 )
 
 func main() {
-	user := storage.NewUser("zero", "foobar2000")
-	fmt.Println(user)
+	storage.NewUser("foobar", "foobar2000")
+
+	http.HandleFunc("/user/", server.RouteUserTree)
+
+	http.ListenAndServe(":8000", nil)
 }
